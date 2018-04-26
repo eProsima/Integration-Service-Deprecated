be7e3eb4575b06e5b33ca185879fbbb0ed3cda45
Luis Gasco <luisgasco@eprosima.com>
2018-04-26 15:40:12 +0200

    std::string to const std::string&

--- src/xmlUtils.h
+++ src/xmlUtils.h
@@ -3,14 +3,14 @@
 
 #include <tinyxml2.h>
 
-static tinyxml2::XMLElement* _assignNextElement(tinyxml2::XMLElement *element, std::string name){
+static tinyxml2::XMLElement* _assignNextElement(tinyxml2::XMLElement *element, const std::string &name){
     if (!element->FirstChildElement(name.c_str())){
         throw 0;
     }
     return element->FirstChildElement(name.c_str());
 }
 
-static tinyxml2::XMLElement* _assignOptionalElement(tinyxml2::XMLElement *element, std::string name){
+static tinyxml2::XMLElement* _assignOptionalElement(tinyxml2::XMLElement *element, const std::string &name){
     return element->FirstChildElement(name.c_str());
 }
 
