b8d3f32b77011c964243579bafad2d25da44d6d8
Luis Gasco <luisgasco@eprosima.com>
2018-04-30 16:06:41 +0200

    Refs #2818: Hierarchical logs

--- src/log/ISLog.h
+++ src/log/ISLog.h
@@ -9,26 +9,29 @@
 
 #ifdef LOG_LEVEL_DEBUG
 #define LOG_DEBUG(x) std::cout << "DEBUG: " << x << std::endl
+#define LOG_INFO(x) std::cout << "INFO: " << x << std::endl
+#define LOG_WARN(x) std::cout << "WARN: " << x << std::endl
+#define LOG_ERROR(x) std::cout << "ERROR: " << x << std::endl
 #else
 #define LOG_DEBUG(x)
-#endif
-
+#ifdef LOG_LEVEL_INFO
+#define LOG_INFO(x) std::cout << "INFO: " << x << std::endl
+#define LOG_WARN(x) std::cout << "WARN: " << x << std::endl
+#define LOG_ERROR(x) std::cout << "ERROR: " << x << std::endl
+#else
+#define LOG_INFO(x)
+#ifdef LOG_LEVEL_WARN
+#define LOG_WARN(x) std::cout << "WARN: " << x << std::endl
+#define LOG_ERROR(x) std::cout << "ERROR: " << x << std::endl
+#else
+#define LOG_WARN(x)
 #ifdef LOG_LEVEL_ERROR
 #define LOG_ERROR(x) std::cout << "ERROR: " << x << std::endl
 #else
 #define LOG_ERROR(x)
 #endif
-
-#ifdef LOG_LEVEL_WARN
-#define LOG_WARN(x) std::cout << "WARN: " << x << std::endl
-#else
-#define LOG_WARN(x)
 #endif
-
-#ifdef LOG_LEVEL_INFO
-#define LOG_INFO(x) std::cout << "INFO: " << x << std::endl
-#else
-#define LOG_INFO(x)
+#endif
 #endif
 
 #define LOG(x) std::cout << "LOG: " << x << std::endl
